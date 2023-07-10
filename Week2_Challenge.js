b7ce431aceaf6859b901160e6c0152e41b2a38fb
Dalton Kujawa <125609395+Dalton-Kujawa@users.noreply.github.com>
2023-07-10 16:27:59 -0500

    Update Week2_Challenge.js

--- Week2_Challenge.js
+++ Week2_Challenge.js
@@ -1,1 +1,16 @@
+function FirstReverse(str) { 
+ let brokenString = str.split("")
+ let reversedString = []
 
+ for (i = brokenString.length-1; i >= 0;i--){
+   reversedString.push(brokenString[i]);
+ }
+  
+  str = reversedString.toString();
+  // code goes here  
+  return str; 
+
+}
+   
+// keep this function call here 
+console.log(FirstReverse(readline()));
