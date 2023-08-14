4ea23a94cd27a6897e18d1ba6076ae61aa07d9c4
Dalton Kujawa <125609395+Dalton-Kujawa@users.noreply.github.com>
2023-08-14 11:13:51 -0500

    Update Week6_Challenge.js

--- Week6_Challenge.js
+++ Week6_Challenge.js
@@ -1,1 +1,12 @@
 
+function charCount(myChar, str) {
+	let arrayOfString = str.split('');
+	let numOfChar = 0;
+	for(let i = 0; i < arrayOfString.length; i++){
+		if(myChar == arrayOfString[i]){
+			numOfChar++;
+		}
+	}
+	return numOfChar;
+	
+}
